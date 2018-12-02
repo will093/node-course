a159e8147a68facffa90a5cf41fa404c7b3cfc09
Will Taylor <willst.willst@gmail.com>
2018-12-02 18:35:02 +0000

    Further templating with hbs

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,22 +3,34 @@ const hbs = require('hbs');
 
 var app = express();
 
+
 // Specify that we want to use hbs as the view engine.
 app.set('view engine', 'hbs');
+
+// Specify where our partials live
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+// Specify our public folder
 app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home page',
         welcomeMessage: 'Welcome!',
-        currentYear: new Date().getFullYear(),
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About page',
-        currentYear: new Date().getFullYear(),
     });
 });
 
