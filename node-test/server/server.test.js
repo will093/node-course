def18f42af14e2a9de9f6c276de34cb6a731479f
Will Taylor <willst.willst@gmail.com>
2018-12-09 20:25:05 +0000

    Describe tutorial

--- node-test/server/server.test.js
+++ node-test/server/server.test.js
@@ -2,23 +2,30 @@ const request = require('supertest');
 const expect = require('expect');
 const app = require('./server').app;
 
-it('should return hello world response.', (done) => {
-    request(app)
-        .get('/')
-        .expect(200)
-        .expect('Hello world!')
-        .end(done);
-});
+describe('server', () => {
 
-it('should return my user', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            name: 'Will',
-            age: 27,
+    describe('/', () => {
+        it('should return hello world response.', (done) => {
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect('Hello world!')
+                .end(done);
+        });
+    })
+
+    describe('/users', () => {
+        it('should return my user', (done) => {
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    name: 'Will',
+                    age: 27,
+                })
+            })
+            .end(done);
         })
     })
-    .end(done);
 })
