23541bd3754deed46645f2fa4c49366a5afe130b
Will Taylor <willst.willst@gmail.com>
2018-12-09 18:53:57 +0000

    Projects page

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({ errorMessage: 'Bad error' });
 });
