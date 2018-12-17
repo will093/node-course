cc5a3ff58e996e4ace4edaec3563ce868acd43f4
Will Taylor <willst.willst@gmail.com>
2018-12-17 23:10:47 +0000

    Implement delete /todos/:id route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -31,16 +31,14 @@ app.get('/todos', (req, res) => {
     })
 })
 
-
 app.get('/todos/:id', (req, res) => {
     const id = req.params.id;
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
-        console.log(todo);
         if (!todo) {
-            res.send(404).send();
+            return res.send(404).send();
         }
         res.send({todo});
     })
@@ -48,6 +46,22 @@ app.get('/todos/:id', (req, res) => {
         res.send(400).send();
     })
 });
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.send(404).send();
+        }
+        res.send({todo});
+    })
+    .catch((e) => {
+        res.send(400).send();
+    })
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
