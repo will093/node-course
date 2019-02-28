e995a18acdcfc97a4d948b6ab85040041368fae2
Will <willtaylor.developer@gmail.com>
2019-02-28 07:51:40 +0000

    Implement generate auth token

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -7,6 +7,7 @@ let { ObjectID } = require('mongodb');
 let mongoose = require('./db/mongoose');
 let { Todo } = require('./models/todo');
 let { User } = require('./models/user');
+let { authenticate } = require('./middleware/authenticate');
 let { pick, isBoolean } = require('lodash');
 
 let app = express();
@@ -102,6 +103,10 @@ app.post('/users', (req, res) => {
     }).catch(e => {
         res.status(400).send(e);
     });
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+   res.send(req.user);
 })
 
 app.listen(port, () => {
