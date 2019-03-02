d516f8c013b79876c21d492719548237c1a11529
Will <willtaylor.developer@gmail.com>
2019-03-02 15:19:58 +0000

    Implement /users/login

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -71,6 +71,25 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    const User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            }); 
+        });
+    });
+}
+
 UserSchema.pre('save', function(next) {
     let user = this;
     
